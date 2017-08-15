f52ad3d1ffcf102ecfc0755bf1dc59db7b60349c
Santhosh-Gundu <30650493+Santhosh-Gundu@users.noreply.github.com>
2017-08-15 20:34:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,21 +112,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-
 //to connect to DB
 var pool = new Pool(config);
-
-app.get('/article-db', function(req, res) {
+app.get('/test-db', function(req, res) {
     
     //make a select request
     //return response with the results
-    pool.query('select * from test' , function(err, result){
+    pool.query('SELECT * FROM test' , function(err, result){
         
         if(err){
             console.log("**********"+err.toString());
@@ -138,6 +130,13 @@ app.get('/article-db', function(req, res) {
     });
     
 });
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
