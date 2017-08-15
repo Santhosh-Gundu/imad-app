1a9dad94ae4eb0032474c4889d86cf51e2d8f657
Santhosh-Gundu <30650493+Santhosh-Gundu@users.noreply.github.com>
2017-08-15 19:12:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var pool = require('pg').Pool;
 var config = {
     user: 'santugundu',
     database: 'santugundu',
-    host: 'localhost',//'http://db.imad.hasura-app.io',
+    host: 'db.imad.hasura-app.io',
     port: '5432',
     password: 'process.env.DB_PASSWORD'
 };
