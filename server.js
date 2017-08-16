5664385168d187181dd4a09ffc39aafd83a3875b
Santhosh-Gundu <30650493+Santhosh-Gundu@users.noreply.github.com>
2017-08-16 18:34:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,19 +92,6 @@ var requests = {
 			return htmlTemplate;
 	}
 
-
-function hash(input, salt){
-    
-    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString('hex');
-}
-
-
-app.get('/hash/:input', function(req, res){
-   var hashString = hash(req.params.input, 'this-is-some-random-string');
-   res.send(hashString);
-    
-});
 var counter = 0;
 app.get('/counter', function (req, res) {
 	counter = counter +1;
@@ -123,6 +110,21 @@ app.get('/submit-name' , function (req, res) {
 	
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
+
+function hash(input, salt){
+    
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+
+app.get('/hash/:input', function(req, res){
+   var hashString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashString);
+    
 });
 
 //to connect to DB
