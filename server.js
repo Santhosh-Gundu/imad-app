bc10207311a302e244a769a73d3a022d4225e166
Santhosh-Gundu <30650493+Santhosh-Gundu@users.noreply.github.com>
2017-08-18 20:29:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get('/submit-name' , function (req, res) {
 });
 	
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'login.html'));
 });
 
 
